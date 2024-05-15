2b219db8d607eaab655e6939e7ae50a8431b9818
Andre Sacilotto <66838864+AndreSacilotto@users.noreply.github.com>
2024-05-15 02:52:39 -0300

    v2

--- src/DiceParser.cs
+++ src/DiceParser.cs
@@ -6,21 +6,21 @@ namespace DiscordDiceRoller;
 public static partial class DiceParser
 {
     public const string ROLL_HELP = @"```md
-## Other
+## Other:
     dr = roll a random positive number
     dh = send this message
 
 ### Items:
     +{} = positive
     -{} = negative
-    {}d{} = dice modifier (default 1d20)
+    {}d{} = dice modifier
+    &{} = against or shift from [don't support shift]
 
 ### Roll Modifiers:
-    !{} = minimal dice roll for explode (default =sides)
-    ?{} = maximun times a explosion can occur (default 1, max is 99)
-    &{} = against or shift from (default 0)
-    l{} = drop lowest X [WIP] (default 0)
-    h{} = drop highest X [WIP] (default 0)
+    !{} = minimal dice roll for explode (default 20)
+    ?{} = maximun times a explosion can occur (default 1)
+    l{} = drop lowest X (default 0)
+    h{} = drop highest X (default 0)
 ```
 ";
 
@@ -44,6 +44,12 @@ public static partial class DiceParser
 
     public static string SingleDiceParse(int sides) => $"` {new Dice(sides).Roll()} `";
 
+    public class RollResults {
+        public required RollDice RollDice { get; init; }
+        public int RollSum { get; set; } = 0;
+        public RollDice.SingleRoll[] Rolls { get; set; } = Array.Empty<RollDice.SingleRoll>();
+    }
+
     public static string RollParse(ReadOnlySpan<char> command)
     {
         // parse commands
@@ -56,10 +62,12 @@ public static partial class DiceParser
         var cmd = sb.Submit().AsSpan();
 
         // do things with commands
-        var diceRolls = new List<RollDice>(1);
+        var diceRolls = new List<RollResults>(1);
         var modifiers = new RollModifier();
 
-        int? shift = null;
+        // TODO: suport negative shifts
+        var shifts = new List<int>();
+
         RollDice? roll = null;
         foreach (var c in ModifiersRegex().EnumerateMatches(cmd))
         {
@@ -68,8 +76,6 @@ public static partial class DiceParser
 
             if (first == 'd' || char.IsAsciiDigit(first))
             {
-                roll = defaultRollDice.DeepCopy();
-
                 // dY, XdY
                 int start = 0;
                 bool isNegative = first == '-'; // -dY, -XdY
@@ -94,9 +100,13 @@ public static partial class DiceParser
                 else
                     return "error: invalid dice: " + match.ToString();
 
-                Dice dice = new(sides, 1, isNegative);
-                roll.diceGroup = new(dice, amount);
-                diceRolls.Add(roll);
+                sides = Math.Clamp(sides, 1, 100);
+                amount = Math.Clamp(amount, 1, 100);
+
+                roll = defaultRollDice.DeepCopy();
+                roll.diceGroup = new(new(sides, 1, isNegative), amount);
+
+                diceRolls.Add(new() { RollDice = roll });
             }
             else if (int.TryParse(match.Slice(1), out var result)) //X{}
             {
@@ -109,7 +119,7 @@ public static partial class DiceParser
                     modifiers.Add(-result);
                     continue;
                     case '&':
-                    shift = result;
+                    shifts.Add(result);
                     continue;
                 }
 
@@ -118,7 +128,7 @@ public static partial class DiceParser
 
                 if (first == '!')
                 {
-                    roll.explodeOn = Math.Abs(result);
+                    roll.explodeOn = Math.Clamp(Math.Abs(result), 0, 100);
                 }
                 else if (first == '?')
                 {
@@ -126,11 +136,11 @@ public static partial class DiceParser
                 }
                 else if (first == 'l')
                 {
-                    roll.dropLow = Math.Abs(result);
+                    roll.dropLow = Math.Clamp(Math.Abs(result), 0, 99);
                 }
                 else if (first == 'h')
                 {
-                    roll.dropHigh = Math.Abs(result);
+                    roll.dropHigh = Math.Clamp(Math.Abs(result), 0, 99);
                 }
                 else
                     return "error: invalid first: " + match.ToString();
@@ -139,66 +149,69 @@ public static partial class DiceParser
                 return "error: invalid command: " + match.ToString();
         }
 
+        // calculate roll items
+        var rollsSum = 0;
+        foreach (var dr in diceRolls)
+        {
+            dr.Rolls = dr.RollDice.CalculateRolls(out var sum).ToArray();
+            dr.RollSum = sum;
+            rollsSum += dr.RollSum;
+        }
+        var final = rollsSum + modifiers.Sum;
+
         // create the response
         var sep = ", ".AsSpan();
-        // TODO: sum rolls intead of making it separted
-        foreach (var diceRoll in diceRolls)
+
+        sb.Append($"` {final,-2} `");
+
+        if (shifts.Count > 0)
         {
-            var rolls = diceRoll.CalculateRolls();
-            var rollSum = 0;
-            foreach (var r in rolls)
-                if (!r.Mask.HasFlag(RollDice.RollMask.Dropped))
-                    rollSum += r.Roll;
-            var final = rollSum + modifiers.Sum;
+            sb.AppendTab().Append("Shift:");
+            foreach (var shift in shifts)
+                sb.AppendSpace().AppendBold().Append(final - shift).AppendBold().Append($" [{shift}]");
+        }
 
-            sb.Append($"` {final} `").AppendLine();
-
-            sb.Append($"` {rollSum} `").AppendSpace();
-            sb.OpenMask();
-            if (rolls.Count > 0)
+        if (diceRolls.Count > 0)
+        {
+            sb.AppendLine().Append($"` {rollsSum,-2} `").AppendTab();
+            foreach (var dr in diceRolls)
             {
-                foreach (var r in rolls)
+                sb.Append($"` {dr.RollSum,-2} `").AppendSpace();
+                sb.OpenMask();
+                foreach (var r in dr.Rolls)
                 {
                     var under = r.Mask.HasFlag(RollDice.RollMask.Explosion);
+                    var italic = r.Mask.HasFlag(RollDice.RollMask.Exploded);
                     var strike = r.Mask.HasFlag(RollDice.RollMask.Dropped);
                     var bold = (r.Mask & (RollDice.RollMask.Minimal | RollDice.RollMask.Maximun)) != 0;
 
-                    if (under)
-                        sb.AppendUnderline();
-                    if (strike)
-                        sb.AppendStrikethrough();
-                    if (bold)
-                        sb.AppendBold();
+                    if (under) sb.AppendUnderline();
+                    if (strike) sb.AppendStrikethrough();
+                    if (bold) sb.AppendBold();
+                    if (italic) sb.AppendItalic();
 
                     sb.Append(r.Roll);
 
-                    if (bold)
-                        sb.AppendBold();
-                    if (strike)
-                        sb.AppendStrikethrough();
-                    if (under)
-                        sb.AppendUnderline();
+                    if (italic) sb.AppendItalic();
+                    if (bold) sb.AppendBold();
+                    if (strike) sb.AppendStrikethrough();
+                    if (under) sb.AppendUnderline();
 
                     sb.Append(sep);
                 }
                 sb.Length -= sep.Length;
+                sb.CloseMask();
             }
-            sb.CloseMask().AppendLine();
+        }
 
-            sb.Append($"` {modifiers.Sum} `").AppendSpace();
+        if (modifiers.Modifiers.Count > 0)
+        {
+            sb.AppendLine().Append($"` {modifiers.Sum,-2} `").AppendTab();
             sb.OpenMask();
-            if (modifiers.Modifiers.Count > 0)
-            {
-                foreach (var m in modifiers.AsString())
-                    sb.Append(m).Append(sep);
-                sb.Length -= sep.Length;
-            }
-            sb.CloseMask().AppendLine();
-
-            if (shift.HasValue)
-                sb.Append("Shift: ").Append(final - shift).AppendLine();
-
-            sb.AppendLine("----------------");
+            foreach (var m in modifiers.AsString())
+                sb.Append(m).Append(sep);
+            sb.Length -= sep.Length;
+            sb.CloseMask();
         }
 
         return sb.ToString();
--- src/dice/RollDice.cs
+++ src/dice/RollDice.cs
@@ -28,16 +28,16 @@ public class RollDice
 {
     public DiceGroup diceGroup;
 
-    public bool canExplode;
     public int explodeOn;
     public int explosionTimes;
 
     public int dropHigh;
     public int dropLow;
 
+    public bool CanExplode() => explosionTimes >= 0;
+
     public RollDice(DiceGroup diceGroup, int explodeOn, int explosionTimes, int dropHigh = 0, int dropLow = 0)
     {
-        canExplode = true;
         this.explodeOn = explodeOn;
         this.explosionTimes = explosionTimes;
 
@@ -48,7 +48,7 @@ public class RollDice
 
     public RollDice(DiceGroup diceGroup, int dropHigh = 0, int dropLow = 0)
     {
-        canExplode = false;
+        explosionTimes = -1;
         this.diceGroup = diceGroup;
         this.dropHigh = dropHigh;
         this.dropLow = dropLow;
@@ -64,25 +64,29 @@ public class RollDice
         None = 0,
         Maximun = 1 << 0,
         Minimal = 1 << 1,
-        Explosion = 1 << 2,
-        Dropped = 1 << 3,
+        Explosion = 1 << 2, // dice from explosion
+        Exploded = 1 << 3, // dice that exploded
+        Dropped = 1 << 4,
     }
 
     public readonly record struct SingleRoll(int Roll, RollMask Mask = RollMask.None);
 
-    public List<SingleRoll> CalculateRolls()
+    public List<SingleRoll> CalculateRolls(out int sum)
     {
         var list = new List<SingleRoll>(diceGroup.amount);
         var dice = diceGroup.dice;
+        var canExplode = CanExplode();
         for (var j = 0; j < diceGroup.amount; j++)
         {
             var flags = RollMask.None;
             var roll = dice.Roll();
             if (roll == dice.max) flags |= RollMask.Maximun;
             if (roll == dice.min) flags |= RollMask.Minimal;
-            list.Add(new(roll, flags));
 
-            if (canExplode)
+            if (canExplode && roll >= explodeOn)
+            {
+                flags |= RollMask.Exploded;
+                list.Add(new(roll, flags));
                 for (int e = 0; e < explosionTimes; e++)
                 {
                     if (roll >= explodeOn)
@@ -96,21 +100,44 @@ public class RollDice
                     else
                         break;
                 }
+            }
+            else
+                list.Add(new(roll, flags));
         }
 
         list.Sort((x, y) => x.Roll.CompareTo(y.Roll));
 
-        for (int i = list.Count - 1; i >= list.Count - dropHigh; i--)
+        // l1 h2 [2 4 8 16 32]_5 (-2) = 3
+        // 0:2 1:4 2:8 4:16 5:32
+
+        sum = 0;
+        var h = list.Count - dropHigh - 1;
+        int explosionToDelete = 0;
+        for (int i = 0; i < list.Count; i++)
         {
             var old = list[i];
-            list[i] = new(old.Roll, old.Mask | RollMask.Dropped);
+            if (i < dropLow || i > h)
+            {
+                if (old.Mask.HasFlag(RollMask.Exploded))
+                    explosionToDelete++;
+                list[i] = new(old.Roll, old.Mask | RollMask.Dropped);
+            }
+            else
+                sum += old.Roll;
         }
 
-        for (int i = 0; i < Math.Min(dropLow, list.Count); i++)
-        {
-            var old = list[i];
-            list[i] = new(old.Roll, old.Mask | RollMask.Dropped);
-        }
+        if (explosionToDelete > 0)
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                var old = list[i];
+                if (list[i].Mask.HasFlag(RollMask.Explosion))
+                {
+                    sum -= old.Roll;
+                    list[i] = new(old.Roll, old.Mask | RollMask.Dropped);
+                    if (--explosionToDelete == 0)
+                        break;
+                }
+            }
 
         return list;
     }
